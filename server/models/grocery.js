de6ce77f2f1f2de25792f8a3d2405dc5fbd96bf5
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-05-20 05:32:25 -0700

    validation

--- server/models/grocery.js
+++ server/models/grocery.js
@@ -2,6 +2,11 @@
 
 module.exports = function(Grocery) {
 
-
+user.validatesPresenceOf('name', 'email');
+  // user.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
+  // user.validatesInclusionOf('gender', {in: ['male', 'female']});
+  // user.validatesExclusionOf('domain', {in: ['www', 'billing', 'admin']});
+  // user.validatesNumericalityOf('age', {int: true});
+  // user.validatesUniquenessOf('email', {message: 'email is not unique'});
 
 };
