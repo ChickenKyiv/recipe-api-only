0996577f1780b0a8cc07ec19d574afe4dedb2977
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-05-26 16:36:18 -0700

    2

--- server/boot/sample-data.js
+++ server/boot/sample-data.js
@@ -84,35 +84,48 @@ function Recipes(app) {
 
 	var recipes = [
 		{ 
-			"customerId": "2", 
-			"ing":[ "6", "7", "8" ],
-			 "id": "1989",
-			  "name": "Crock Pot Roast",
-			  "ingredients": 
-			  [{"quantity": "1",
-			  "name": " beef roast",
-			  "type": "Meat"},
-			  {"quantity": "1 package",
-			  "name": "brown gravy mix",
-			  "type": "Baking"},
-			  {"quantity": "1 package",
-			  "name": "dried Italian salad dressing mix",
-			  "type": "Condiments"},
-			  {"quantity": "1 package",
-			  "name": "dry ranch dressing mix",
-			  "type": "Condiments"},
-			  {"quantity": "1/2 cup",
-			  "name": "water",
-			  "type": "Drinks"}],
-			  "directions": [
+			customerId: "2", 
+			ing:[ "6", "7", "8" ],
+			 id: "1989",
+			  name: "Crock Pot Roast",
+			  ingredients: 
+			  [{
+			  	quantity: "1",
+			  name: " beef roast",
+			  type: "Meat"
+			},
+			  {
+			  	quantity: "1 package",
+			  name: "brown gravy mix",
+			  type: "Baking"
+			},
+			  {
+			  	quantity: "1 package",
+			  name: "dried Italian salad dressing mix",
+			  type: "Condiments"
+			},
+			  {
+			  	quantity: "1 package",
+			  name: "dry ranch dressing mix",
+			  type: "Condiments"
+			},
+			  {
+			  	quantity: "1/2 cup",
+			  name: "water",
+			  type: "Drinks"
+			}
+			],
+			  directions: [
 			 "Place beef roast in crock pot.",
 			  "Mix the dried mixes together in a bowl and sprinkle over the roast.",
-			  "Pour the water around the roast.","Cook on low for 7-9 hours."],  
+			  "Pour the water around the roast.",
+			  "Cook on low for 7-9 hours."
+			  ],  
 			  prep_time    :"30min",
 			  total_time   :"3h",
-			  "recipe_yield" :"8",
-			   "img": "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg","url": "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208",
-			   "created_at":"December 17, 2003 03:24:00",
+			  recipe_yield :"8",
+			   img: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg","url": "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208",
+			   created_at:"December 17, 2003 03:24:00",
 			   updated_at:"December 17, 2003 03:24:00"
 			},
 		{ 
@@ -123,16 +136,26 @@ function Recipes(app) {
 			  img: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",url: "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208",
 			  ingredients: 
 			  [
-			  {quantity: "1",
-			  name: " beef roast"},
-			  {quantity: "1 package",
-			  name: "brown gravy mix"},
-			  {quantity: "1 package",
-			  name: "dried Italian salad dressing mix"},
-			  {quantity: "1 package",
-			  name: "dry ranch dressing mix"},
-			  {quantity: "1/2 cup",
-			  name: "water"}
+			  {
+			  	quantity: "1",
+			  name: " beef roast"
+			},
+			  {
+			  	quantity: "1 package",
+			  name: "brown gravy mix"
+			},
+			  {
+			  	quantity: "1 package",
+			  name: "dried Italian salad dressing mix"
+			},
+			  {
+			  	quantity: "1 package",
+			  name: "dry ranch dressing mix"
+			},
+			  {
+			  	quantity: "1/2 cup",
+			  name: "water"
+			}
 			  ],
 			  directions: 
 			  [
