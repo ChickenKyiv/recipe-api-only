0ac3a51b726cbe26668356aec2e2eee456486014
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-05-26 17:07:42 -0700

    1

--- server/boot/sample-data.js
+++ server/boot/sample-data.js
@@ -4,7 +4,85 @@ var async      = require('async');
 
 module.exports = function(app) {
 
-  var User        = app.models.user;
+ //  var User        = app.models.user;
+ //  var Role        = app.models.Role;
+ //  var RoleMapping = app.models.RoleMapping;
+
+ //  var accounts = [
+	// {
+	//   name: 'john',	
+	//   email: 'john.doe@ibm.com',
+	//   password: 'john',
+	//   created_at: new Date(),
+	//   updated_at: new Date(),
+	// },
+	// {
+	//   name: 'jane',
+	//   email: 'jane.doe@ibm.com',
+	//   password: 'jane',
+	//   created_at: new Date(),
+	//   updated_at: new Date(),
+	// },
+	// {
+	//   name: 'admin',
+	//   email: 'admin@ibm.com',
+	//   password: 'admin',
+	//   created_at: new Date(),
+	//   updated_at: new Date(),
+
+	// }
+ //  ];
+
+	// accounts.forEach(function(element) {
+	//     // console.log(element);
+
+	//     User.findOrCreate({
+	//       where: {
+	//         name: element.name,
+	//         email: element.email,
+	//       }
+	//     }, element,
+	//     function (err, user) {
+	//       if (err) throw err;
+	//       // console.log("+ " + log.id);
+	//       // callback();
+
+	//       if (user.name == 'admin'){
+ //      		// create the admin role
+
+ //      			console.log(user);
+
+ //      			// commented due to laziness moving this code to automigrate.
+	// 		    Role.create({
+	// 		      name: 'admin'
+	// 		    }, function(err, role) {
+	// 		      if (err) throw err;
+
+	// 		      console.log('Created role:', role);
+
+	// 		      //make admin an admin role
+	// 		      role.principals.create({
+	// 		        principalType: RoleMapping.USER,
+	// 		        principalId: user.id 
+	// 		      }, function(err, principal) {
+	// 		        if (err) throw err;
+
+	// 		        console.log('Created principal:', principal);
+	// 		      });
+	// 		    });
+
+	//       }
+
+
+	//     });
+
+	// });
+
+};
+
+function importUsers(app){
+	
+	var User        = app.models.user;
   var Role        = app.models.Role;
   var RoleMapping = app.models.RoleMapping;
 
@@ -77,8 +155,8 @@ module.exports = function(app) {
 	    });
 
 	});
+}
 
-};
 
 function Recipes(app) {
 
