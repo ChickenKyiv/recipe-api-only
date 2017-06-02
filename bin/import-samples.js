8709e0717782a006d9058ad457c9db0c007e0635
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-01 23:24:19 -0700

    123

--- bin/import-samples.js
+++ bin/import-samples.js
@@ -12,6 +12,8 @@ let menus       = require(path.resolve(__dirname, 'sample-menus-data'));
 
 let ingredients = require(path.resolve(__dirname, 'sample-ingredients-data'));
 
+let groceries   = require(path.resolve(__dirname, 'sample-grocery-data'));
+
 var User        = app.models.UserModel;
 var Role        = app.models.Role;
 var RoleMapping = app.models.RoleMapping;
@@ -23,59 +25,66 @@ var Ingredient  = app.models.IngredientModel;
 var Menu        = app.models.MenuModel;
 
 
-accounts(function(array){
+// accounts(function(array){
 
 
 
-	User.create(array)
-		.then(function(users){
-			// console.log(users);
+// 	User.create(array)
+// 		.then(function(users){
+// 			// console.log(users);
 
-			User.findOne({fields:'id', where: { name:'admin' }})
-				.then(function(result){
+// 			User.findOne({fields:'id', where: { name:'admin' }})
+// 				.then(function(result){
 					
-					Role.create({ name:'admin' })
-						.then(function(role){
+// 					Role.create({ name:'admin' })
+// 						.then(function(role){
 
-							role.principals.create({
-						        principalType: RoleMapping.USER,
-						        principalId: result.id
-						    }, function(err, principal){
-						    	console.log('Principal', principal);
-						    });
-						})
-						.catch(function(err){
-							throw err;
-						})
-				})		
+// 							role.principals.create({
+// 						        principalType: RoleMapping.USER,
+// 						        principalId: result.id
+// 						    }, function(err, principal){
+// 						    	console.log('Principal', principal);
+// 						    });
+// 						})
+// 						.catch(function(err){
+// 							throw err;
+// 						})
+// 				})		
 
-		})
-		.catch(function(err){
-			throw err;
-		})
-
-
-});
+// 		})
+// 		.catch(function(err){
+// 			throw err;
+// 		})
 
 
-videos(function(array){
-
-	Video.create(array)
-		 .then(function(videos){
-
-		 	User.findOne({fields:'id', where: { name:'admin' }})
-				.then(function(result){
-
-					videos.forEach(function(video){
-				 		video.updateAttribute('userId', result.id);
-				 	})
+// });
 
 
-				});
+// videos(function(array){
+
+// 	Video.create(array)
+// 		 .then(function(videos){
+
+// 		 	User.findOne({fields:'id', where: { name:'admin' }})
+// 				.then(function(result){
+
+// 					videos.forEach(function(video){
+// 				 		video.updateAttribute('userId', result.id);
+// 				 	})
+
+
+// 				});
 
 		 		
-		 		console.log(videos);
+// 		 		console.log(videos);
 
-		 })
+// 		 })
 
+// });
+
+
+groceries(function(array){
+
+
+	
 });
--- bin/sample-grocery-data.js
+++ bin/sample-grocery-data.js
@@ -5,6 +5,7 @@ module.exports = function getSamplesData (cb){
 	var grocery = [
 	{
 		ing:["6", "7", "8" ],
+
 		departments: [
 		{
 			"id": "1",
@@ -12,23 +13,24 @@ module.exports = function getSamplesData (cb){
 		  "items": [
 		  {
 		  	id: "6","name": "black pepper",
-		  recipeId: ["1", "2"],
-		  type: "simple",
-		  units: "1 teaspoon",
-		  done: "0",
-		  delete: "0"
+		  	recipeId: ["1", "2"],
+		  	type: "simple",
+		  	units: "1 teaspoon",
+		  	done: "0",
+		  	delete: "0"
 			}
 		  ]
-		},{
+		},
+		{
 			id: "2",
-			 name: "BAKING",
-			  items: [{
-			  	"id": "6",
-			  name: "black pepper",
-			  recipeId: ["1", "2"],
-			  type: "simple",
-			  units: "1 teaspoon",
-			  done: "0","delete": "0"
+			name: "BAKING",
+			items: [{
+				"id": "6",
+				name: "black pepper",
+				recipeId: ["1", "2"],
+				type: "simple",
+				units: "1 teaspoon",
+				done: "0","delete": "0"
 			}
 			]
 		}
