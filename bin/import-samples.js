cf792b105100d80103777190eb80ea7d08d44bd2
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-12 02:16:58 -0700

    upd

--- bin/import-samples.js
+++ bin/import-samples.js
@@ -10,11 +10,11 @@ let recipes     = require(path.resolve(__dirname, 'sample-recipes-data'));
 
 let menus       = require(path.resolve(__dirname, 'sample-menus-data'));
 
-// let ingredients = require(path.resolve(__dirname, 'sample-ingredients-data'));
+let ingredients = require(path.resolve(__dirname, 'sample-ingredients-data'));
 
-// let groceries   = require(path.resolve(__dirname, 'sample-grocery-data'));
+let groceries   = require(path.resolve(__dirname, 'sample-grocery-data'));
 
-// let departments = require(path.resolve(__dirname, 'sample-departments-data'));
+let departments = require(path.resolve(__dirname, 'sample-departments-data'));
 
 
 var User        = app.models.UserModel;
@@ -23,96 +23,73 @@ var User        = app.models.UserModel;
 
 
 var Recipe      = app.models.RecipeModel; 
-// var Ingredient  = app.models.IngredientModel;
+var Ingredient  = app.models.IngredientModel;
 
 var Menu        = app.models.MenuModel;
 var Grocery     = app.models.GroceryModel;
 
 var Department  = app.models.DepartmentModel;
 
-// accounts(function(array){
+accounts(function(array){
 
 
 
-// 	User.create(array)
-// 		.then(function(users){
-// 			// console.log(users);
+	User.create(array)
+		.then(function(users){
+			
+			User.assign();
 
-// 			// User.findOne({fields:'id', where: { name:'admin' }})
-// 			// 	.then(function(result){
-					
-// 			// 		Role.create({ name:'admin' })
-// 			// 			.then(function(role){
-
-// 			// 				role.principals.create({
-// 			// 			        principalType: RoleMapping.USER,
-// 			// 			        principalId: result.id
-// 			// 			    }, function(err, principal){
-// 			// 			    	console.log('Principal', principal);
-// 			// 			    });
-// 			// 			})
-// 			// 			.catch(function(err){
-// 			// 				throw err;
-// 			// 			})
-// 			// 	});		
-// 			User.assign();
-
-// 		})
-// 		.catch(function(err){
-// 			throw err;
-// 		})
+		})
+		.catch(function(err){
+			throw err;
+		})
 
 
-// });
+});
 
 
 
-// ingredients(function(array){
-
-// 	Ingredient.create(array)
-// 	.then(function(ingredients){
-// 		// just first recipe
-// 		Recipe.findOne({
-// 			fields:'id',
-// 			where: { title:'Crock Pot Roast1' }
-// 		})
-// 		.then(function(result){
-
-// 			ingredients.forEach(function(ingredients){
-// 			 	ingredients.updateAttribute('userId', result.id);
-// 			})
 
 
-// 		});
+recipes(function(array){
 
-// 		Recipe.addIngredients(ingredients);
+
+	Recipe.create(array)
+		 .then(function(recipes){
+
+		 }).catch(function(err){throw err;});
+});
+
+
+ingredients(function(array){
+
+	Ingredient.create(array)
+	.then(function(ingredients){
+		// just first recipe
+		Recipe.findOne({
+			fields:'id',
+			where: { title:'Crock Pot Roast1' }
+		})
+		.then(function(result){
+
+			ingredients.forEach(function(ingredients){
+			 	ingredients.updateAttribute('userId', result.id);
+			})
+
+
+		});
+
+		Recipe.addIngredients(ingredients);
 	
-// 		console.log(ingredients);
+		console.log(ingredients);
 
-// 	})
-// 	.catch(function(err){
-// 		throw err;
-// 	})
+	})
+	.catch(function(err){
+		throw err;
+	})
 
-// });
+});
 
-
-// recipes(function(array){
-
-// 	// console.log(Recipe);
-
-
-// 	Recipe.create(array)
-// 		 .then(function(recipes){
-
-		 	
-
-// 			// User.addVideos(videos);	
-		 		
-// 		 		// console.log(recipes);
-
-// 		 }).catch(function(err){throw err;});
-// });
 
 menus(function(array) {
 	
