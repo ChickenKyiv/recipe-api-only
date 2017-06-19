e419f657f74f01391d6dc3d5a6ba9ff27dcbc557
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-19 00:19:53 -0700

    1

--- bin/allergy.js
+++ bin/allergy.js
@@ -1,1 +1,48 @@
 'use strict';
+
+
+    
+
+     var allergy     = [
+     {
+     	"id":"393","shortDescription":"Gluten-Free","longDescription":"Gluten-Free",
+     	"searchValue":"393^Gluten-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"394","shortDescription":"Peanut-Free","longDescription":"Peanut-Free",
+     	"searchValue":"394^Peanut-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"398","shortDescription":"Seafood-Free","longDescription":"Seafood-Free",
+     	"searchValue":"398^Seafood-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"399","shortDescription":"Sesame-Free","longDescription":"Sesame-Free",
+     	"searchValue":"399^Sesame-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"400","shortDescription":"Soy-Free","longDescription":"Soy-Free",
+     	"searchValue":"400^Soy-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"396","shortDescription":"Dairy-Free","longDescription":"Dairy-Free",
+     	"searchValue":"396^Dairy-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"397","shortDescription":"Egg-Free","longDescription":"Egg-Free",
+     	"searchValue":"397^Egg-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"401","shortDescription":"Sulfite-Free","longDescription":"Sulfite-Free",
+     	"searchValue":"401^Sulfite-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"395","shortDescription":"Tree Nut-Free","longDescription":"Tree Nut-Free",
+     	"searchValue":"395^Tree Nut-Free","type":"allergy","localesAvailableIn":["en-US"]
+     },
+     {
+     	"id":"392","shortDescription":"Wheat-Free","longDescription":"Wheat-Free",
+     	"searchValue":"392^Wheat-Free","type":"allergy","localesAvailableIn":["en-US"]
+     }
+     ];
+    
--- bin/diet.js
+++ bin/diet.js
@@ -1,1 +1,37 @@
 'use strict';
+
+
+
+
+     var diet        = [
+     {
+     	"id":"388","shortDescription":"Lacto vegetarian","longDescription":"Lacto vegetarian",
+     	"searchValue":"388^Lacto vegetarian",
+     "type":"diet","localesAvailableIn":["en-US"]
+ },
+     {
+     	"id":"389","shortDescription":"Ovo vegetarian","longDescription":"Ovo vegetarian",
+     	"searchValue":"389^Ovo vegetarian",
+     "type":"diet","localesAvailableIn":["en-US"]
+ },
+     {
+     	"id":"390","shortDescription":"Pescetarian","longDescription":"Pescetarian",
+     	"searchValue":"390^Pescetarian",
+     "type":"diet","localesAvailableIn":["en-US"]
+ },
+     {
+     	"id":"386","shortDescription":"Vegan","longDescription":"Vegan",
+     	"searchValue":"386^Vegan",
+     "type":"diet","localesAvailableIn":["en-US"]
+ },
+     {
+     	"id":"387","shortDescription":"Lacto-ovo vegetarian","longDescription":"Vegetarian",
+     	"searchValue":"387^Lacto-ovo vegetarian",
+     "type":"diet","localesAvailableIn":["en-US"]
+ },
+     {
+     	"id":"403","shortDescription":"Paleo","longDescription":"Paleo",
+     	"searchValue":"403^Paleo",
+     "type":"diet","localesAvailableIn":["en-US"]
+ }
+     ];
