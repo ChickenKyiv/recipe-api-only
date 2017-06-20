9ea50bb9f4c740c2ba6cc59fe5637cb61581b0ad
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-20 14:28:22 -0700

    1

--- bin/holidays.js
+++ bin/holidays.js
@@ -1,83 +1,76 @@
 'use strict';
 
 
+module.exports = function getSampleData (){
+   
      var holidays    = [
-     {"id":"holiday-christmas","name":"Christmas","type":"holiday","description":"Christmas",
+     {
+          "id":"holiday-christmas","name":"Christmas","type":"holiday","description":"Christmas",
      "searchValue":"holiday^holiday-christmas","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-thanksgiving","name":"Thanksgiving","type":"holiday","description":"Thanksgiving",
+     },
+     {
+          "id":"holiday-thanksgiving","name":"Thanksgiving","type":"holiday","description":"Thanksgiving",
      "searchValue":"holiday^holiday-thanksgiving","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-summer","name":"Summer","type":"holiday","description":"Summer",
+     },
+     {
+          "id":"holiday-summer","name":"Summer","type":"holiday","description":"Summer",
      "searchValue":"holiday^holiday-summer","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-fall","name":"Fall","type":"holiday","description":"Fall",
+     },
+     {
+          "id":"holiday-fall","name":"Fall","type":"holiday","description":"Fall",
      "searchValue":"holiday^holiday-fall","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-new-year","name":"New Year","type":"holiday","description":"New Year",
+     },
+     {
+          "id":"holiday-new-year","name":"New Year","type":"holiday","description":"New Year",
      "searchValue":"holiday^holiday-new-year","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-super-bowl","name":"Game Day","type":"holiday","description":"Game Day",
+     },
+     {
+          "id":"holiday-super-bowl","name":"Game Day","type":"holiday","description":"Game Day",
      "searchValue":"holiday^holiday-super-bowl","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-winter","name":"Winter","type":"holiday","description":"Winter",
+     },
+     {
+          "id":"holiday-winter","name":"Winter","type":"holiday","description":"Winter",
      "searchValue":"holiday^holiday-winter","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-spring","name":"Spring","type":"holiday","description":"Spring",
+     },
+     {
+          "id":"holiday-spring","name":"Spring","type":"holiday","description":"Spring",
      "searchValue":"holiday^holiday-spring","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-halloween","name":"Halloween","type":"holiday","description":"Halloween",
+     },
+     {
+          "id":"holiday-halloween","name":"Halloween","type":"holiday","description":"Halloween",
      "searchValue":"holiday^holiday-halloween","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-valentines-day","name":"Valentine's Day","type":"holiday","description":"Valentine's Day",
+     },
+     {
+          "id":"holiday-valentines-day","name":"Valentine's Day","type":"holiday","description":"Valentine's Day",
      "searchValue":"holiday^holiday-valentines-day","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-hanukkah","name":"Hanukkah","type":"holiday","description":"Hanukkah",
+     },
+     {
+          "id":"holiday-hanukkah","name":"Hanukkah","type":"holiday","description":"Hanukkah",
      "searchValue":"holiday^holiday-hanukkah","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-passover","name":"Passover","type":"holiday","description":"Passover",
+     },
+     {
+          "id":"holiday-passover","name":"Passover","type":"holiday","description":"Passover",
      "searchValue":"holiday^holiday-passover","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-easter","name":"Easter","type":"holiday","description":"Easter",
+     },
+     {
+          "id":"holiday-easter","name":"Easter","type":"holiday","description":"Easter",
      "searchValue":"holiday^holiday-easter","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-st-patricks-day","name":"St. Patrick's Day","type":"holiday","description":"St. Patrick's Day",
+     },
+     {
+          "id":"holiday-st-patricks-day","name":"St. Patrick's Day","type":"holiday","description":"St. Patrick's Day",
      "searchValue":"holiday^holiday-st-patricks-day","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-chinese-new-year","name":"Chinese New Year","type":"holiday","description":"Chinese New Year",
+     },
+     {
+          "id":"holiday-chinese-new-year","name":"Chinese New Year","type":"holiday","description":"Chinese New Year",
      "searchValue":"holiday^holiday-chinese-new-year","localesAvailableIn":["en-US"]
- },
-     {"id":"holiday-4th-of-july","name":"4th of July","type":"holiday","description":"4th of July",
+     },
+     {
+          "id":"holiday-4th-of-july","name":"4th of July","type":"holiday","description":"4th of July",
      "searchValue":"holiday^holiday-4th-of-july","localesAvailableIn":["en-US"]
- }
+     }
      ];
 
-     
-
-function getSampleData (cb){
-
-	var ingredient = [	
-		{
-		  username: 'john',	
-		  email: 'john.doe@ibm.com',
-		  password: 'john1',
-
-		},
-		{
-		  username: 'jane',
-		  email: 'jane.doe@ibm.com',
-		  password: 'jane1',
-		},
-		{
-		  username: 'admin',
-		  email: 'admin@ibm.com',
-		  password: 'admin',
-
-		}
-  	];
-
-  	return cb(accounts);
+    
+     return holidays;
 
 };
-
-module.exports = getSampleData();
