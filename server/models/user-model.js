ed9343de76f4895d488b3eace9280f327d5d07bd
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-07-04 20:50:07 -0700

    email notificaiotns

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -2,38 +2,7 @@
 
 module.exports = function(UserModel) {
 
-  // Usermodel.validatesPresenceOf('username');
-  // Usermodel.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
-    
-  // var re = /^(([^<>()[\]\\.,;:\s@\"]-(\.[^<>()[\]\\.,;:\s@\"]-)*)|(\".-\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]-\.)-[a-zA-Z]{2,}))$/;
-
-  // UserModel.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
-  // if (!(UserModel.settings.realmRequired || UserModel.settings.realmDelimiter)) {
-  //   UserModel.validatesUniquenessOf('email', {message: 'Email already exists'});
-  //   UserModel.validatesUniquenessOf('username', {message: 'User already exists'});
-  // }
-
-
-
-  UserModel.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
   
-  UserModel.validatesUniquenessOf('email', {message: 'email is not unique'});
-// Usermodel.validatesNumericalityOf('start', {int: true});
-
-  UserModel.observe("before save", function updateTimestamp(ctx, next) {
- 
-    	if( ctx.isNewInstance ){
-    		ctx.instance.created_at = new Date();
-    		ctx.instance.updated_at = new Date();
-    	} 
-    	
-    	next();
-  });
-
-  UserModel.observe('update', function(ctx, next){
-  	ctx.instance.updated_at = new Date();
-  	next();
-  });
 
   UserModel.log = function(userId, options) {
     
@@ -121,46 +90,124 @@ module.exports = function(UserModel) {
   });
 
 
+  // UserModel.validatesPresenceOf('username');
+  // UserModel.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
+
+  // @TODO finish this 
+  // var re = /^(([^<>()[\]\\.,;:\s@\"]-(\.[^<>()[\]\\.,;:\s@\"]-)*)|(\".-\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]-\.)-[a-zA-Z]{2,}))$/;
+
+  // UserModel.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
+  // if (!(UserModel.settings.realmRequired || UserModel.settings.realmDelimiter)) {
+  //   UserModel.validatesUniquenessOf('email', {message: 'Email already exists'});
+  //   UserModel.validatesUniquenessOf('username', {message: 'User already exists'});
+  // }
 
 
+
+  UserModel.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
   
+  UserModel.validatesUniquenessOf('email', {message: 'email is not unique'});
+// Usermodel.validatesNumericalityOf('start', {int: true});
+
+  UserModel.afterRemote("create", function(ctx, userInstance, next){
+
+    console.log('> user.afterRemote triggered');
+
+    var emailOptions = {
+
+      type    : 'email',
+      to      : userInstance.email,
+      from    : 'noreply@github.com',
+      subject : 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/account/verify.pug'),
+      redirect: '/verified',
+      user: user
+    };
+
+    userInstance.verify(options, function(err, response, next){
+
+      if (err) {
+        UserModel.deleteById(userInstance.id);
+        return next(err);
+      }
+
+      ctx.res.render('account/response', {        
+        title  : 'Signed up successfully',
+        content: 'Please check your email and click on the verification link ' +
+        'before loggin in.',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
 
 
+    });
 
-  // VideoModel.listVideosByUser = function(userId, cb){
-  //   var UserModel = VideoModel.app.models.UserModel;
 
-  //   UserModel.exists(userId, function(err, user){
-  //     if(err){ cb(err); }
+  });
 
-  //     VideoModel.find({
-  //       where: {
-  //         userId: userId
-  //       },
-  //       fields: [
-  //         'title', 'url', 'desc',
-  //         'start', 'end', 'step'
-  //       ]       
-        
-  //     }, cb);
-  //   });
+  UserModel.afterRemote("prototype.verify", function(ctx, userInstance, next){
 
-  // };
+    ctx.res.render('account/response',{
+      title: 'A Link to reverify your identity has been sent '+
+        'to your email successfully',
+      content: 'Please check your email and click on the verification link '+
+        'before loggin in',
+      redirectTo: '/', 
+      redirectToLinkText: 'Log in'
+    });
 
-  // VideoModel.remoteMethod('listVideosByUser', {
-  //   accepts: {
-  //     arg: 'id',
-  //     type: 'string'
-  //   },
-  //   returns: {
-  //     arg: 'videos',
-  //     type: 'array'
-  //   },
-  //   http: {
-  //     path: '/view-videos',
-  //     verb: 'get'
-  //   }
-  // });
+  });
+
+  UserModel.on('resetPasswordRequest', function(info){
+
+    var url  = 'http://' + config.host + ':' + config.port + '/reset-password';
+    var html = 'Click <a href="' + url + '?access_token=' + 
+      info.accessToken.id+'">here</a> to reset your password';
+
+    UserModel.app.models.EmailModel.send({
+      to     : info.email,
+      from   : info.email,
+      subject: 'Password reset',
+      html   :html
+    }, function(err){
+      if (err) return console.log('> error sending password reset email');
+      console.log('> sending password reset email to:', info.email);
+    });  
+
+  });
+
+  UserModel.afterRemote('changePassword', function(ctx, userInstance, next){
+    context.res.render('account/response', {
+      title      : 'Password changed successfully',
+      content    : 'Please login again with new password',
+      redirectTo : '/',
+      redirectToLinkText: 'Log in'
+    });
+  });
+
+  UserModel.afterRemote('setPassword', function(ctx, userInstance, next){
+    ctx.res.render('account/response', {
+      title      : 'Password reset success',
+      content    : 'Your password has been reset successfully',
+      redirectTo : '/',
+      redirectToLinkText: 'Log in'
+    });
+  });
+
+  UserModel.observe("before save", function updateTimestamp(ctx, next) {
+ 
+      if( ctx.isNewInstance ){
+        ctx.instance.created_at = new Date();
+        ctx.instance.updated_at = new Date();
+      } 
+      
+      next();
+  });
+
+  UserModel.observe('update', function(ctx, next){
+    ctx.instance.updated_at = new Date();
+    next();
+  });
 
 
 };
