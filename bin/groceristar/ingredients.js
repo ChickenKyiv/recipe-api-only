3a87869237b72c3bbe7d369c2871e6681bd8a4f8
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-01-10 16:14:49 +0300

    +

--- bin/groceristar/ingredients.js
+++ bin/groceristar/ingredients.js
@@ -979,44 +979,6 @@ var relation2   = 'ingredientIds';
 
 
 
-
-
-// function attachIngredientsToRecipes(ingredients, recipes){
-
-//   var first  = ingredients.slice(0, 2);
-//   var second = ingredients.slice(1, 3);
-//   console.log(first)  ;
-//   console.log(second) ;
-
-//   var one = idsOnly(first);
-//   var two = idsOnly(second);
-
-//   recipes[0].updateAttribute(relation, one);
-
-//   recipes[1].updateAttribute(relation, two);
-
-//   console.log(recipes);
-
-
-//   // // only first 10 elements attach
-//   // var first10  = arrayWithIds.slice(0, 10);
-//   // var second10 = arrayWithIds.slice(11, 21);
-
-//   // recipes.forEach(function(recipe, index){
-
-//   //  if (index % 2 === 0){
-//   //    recipe.updateAttribute('ingredients', first10);
-//   //  } else {
-//   //    recipe.updateAttribute('ingredients', second10);
-//   //  }
-
-//   //  // recipe.updateAttribute('ingredients', arrayWithIds);
-
-//   // });
-// };
-
-
-
 function attachIngredientsToGroceries(ingredients, groceries){
  var arrayWithIds = idsOnly(ingredients);
 
