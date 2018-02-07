54191846960a541812e96a3a86764a2ad12fbedf
Arthur Tkachenko <atherdon@gmail.com>
2018-02-07 11:31:33 +0200

    updated and added other recipes

--- bin/recipes/ingredients.js
+++ bin/recipes/ingredients.js
@@ -5,82 +5,181 @@ let table_name = 'Ingredient'
 // this indredients haven't nice departments, so i attach them only to department[0]
 const get = (department_id) => {
     let data
-    data[0]     = [
+
+    data[0] = [
+      {
+        name: "medium russet potatoes, sliced into sticks",
+        departmentId: department_id
+      },
+      {
+        name: "tablespoons vegetable oil, divided",
+        departmentId: department_id
+      },
+      {
+        name: "salt & freshly ground black pepper, to taste",
+        departmentId: department_id
+      },
+      {
+        name: "scallions",
+        departmentId: department_id
+      },
+      {
+        name: "ounces cheese curds or 6 ounces fresh mozzarella cheese, diced",
+        departmentId: department_id
+      }
+    ];
+
+    data[1] = [
+      {
+        name: "1 box Pillsbury™ refrigerated pie crusts, softened as directed on box",
+        departmentId: department_id
+      },
+      {
+        name: "6 cups thinly sliced, peeled apples (6 medium)",
+        departmentId: department_id
+      },
+      {
+        name: "3/4 cup sugar",
+        departmentId: department_id
+      },
+      {
+        name: "2 tablespoons all-purpose flour",
+        departmentId: department_id
+      },
+      {
+        name: "3/4 teaspoon ground cinnamon",
+        departmentId: department_id
+      },
+      {
+        name: "1/4 teaspoon salt",
+        departmentId: department_id
+      },
+      {
+        name: "1/8 teaspoon ground nutmeg",
+        departmentId: department_id
+      },
+      {
+        name: "1 tablespoon lemon juice"
+        departmentId: department_id
+      },
+    ];
+
+    data[2] = [
+      {
+        name : "apple",
+        departmentId : department_id
+      },
+      {
+        name : "banana",
+        departmentId : department_id
+      },
+      {
+        name : "orange",
+        departmentId : department_id
+      },
+      {
+        name : "grapes",
+        departmentId : department_id
+      }
+    ];
+
+    data[3] = [
+      {
+        name: " beef roast",
+        departmentId : department_id
+      },
+      {
+        name: "brown gravy mix",
+        departmentId : department_id
+      },
+      {
+        name: "dried Italian salad dressing mix",
+        departmentId : department_id
+      },
+      {
+        name "dry ranch dressing mix",
+        departmentId : department_id
+      },
+      {
+        name: "water",
+        departmentId : department_id
+      }
+    ];
+
+    data[4]     = [
 			{
 	  		name: "black pepper",
+        departmentId: department_id
+	  	},
+	  	{
+	      name: "extra-virgin olive oil",
+	      departmentId: department_id
+	    },
+	    {
+        name: "kosher salt",
 	      departmentId: department_id
 	  	},
 	  	{
-	        name: "extra-virgin olive oil",
+        name: "kosher salt and freshly ground black",
 	      departmentId: department_id
-	    },
-	    {
-	     name: "kosher salt",
-	      departmentId: department_id
-	  	},
-	  	{
-	       name: "kosher salt and freshly ground black",
-	      departmentId: department_id
-	  	},
-
-
-
-	    {
-	      name: "Celery",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Corn",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Cucumbers",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Lettuce / Greens",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Mushrooms",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Onions",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Peppers",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Potatoes",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Spinach",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Squash",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Zucchini",
-	      departmentId: department_id
-	    },
-	    {
-	      name: "Tomatoes*",
-	      departmentId: department_id
-	    }
+	  	}
 
      ];
 
 
-     data[1] = [
-       { },
-       { },
-     ];
+
+
+     // data[3] = [
+     //  {
+ 	   //    name: "Celery",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Corn",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Cucumbers",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Lettuce / Greens",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Mushrooms",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Onions",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Peppers",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Potatoes",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Spinach",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Squash",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Zucchini",
+ 	   //    departmentId: department_id
+ 	   //  },
+ 	   //  {
+ 	   //    name: "Tomatoes*",
+ 	   //    departmentId: department_id
+ 	   //  }
+     //
+     // ];
 
 
   	return data;
--- bin/recipes/recipes-extended.js
+++ bin/recipes/recipes-extended.js
@@ -23,333 +23,63 @@ const get = () => {
 
     var data     = [
 
-
-
-                {
-                    "id": "2",
-                    //"name": "Montreal Style Poutine",
-                    "imageURL": "http://img.sndimg.com/food/image/upload/h_479,w_639,c_fit/v1/img/recipes/51/78/46/BKN9rVETnqVjW3zddIJ1_0S9A4664.jpg",
-                    "originalURL": "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208",
-
-                    "title"   : "Baked Chicken Chimichangas, Borracho Beans",
-                    "ingredients": [
-                        {
-
-                            "unit_type" : "simple",
-                            "quantity"  : "3",
-                            "unit_name" : "medium russet potatoes, sliced into sticks",
-
-                            "category"  : {
-                                "id" : "2",
-                                "name"   : "Baking",
-                            }
-
-
-                        },
-                        {
-
-                            "unit_type" : "simple",
-                            "quantity"  : "2",
-                            "unit_name" : "tablespoons vegetable oil, divided",
-
-                            "category"  : {
-                                "id" : "2",
-                                "name"   : "Baking",
-                            }
-
-
-                        },
-                        {
-
-                            "unit_type" : "simple",
-                            "quantity"  : "1",
-                            "unit_name" : "salt & freshly ground black pepper, to taste",
-
-                            "category"  : {
-                                "id" : "2",
-                                "name"   : "Baking",
-                            }
-
-
-                        },
-                        {
-
-                            "unit_type" : "simple",
-                            "quantity"  : "2 chopped",
-                            "unit_name" : "scallions",
-
-                            "category"  : {
-                                "id" : "1",
-                                "name"   : "Meat",
-                            }
-
-
-                        },
-                        {
-
-                            "unit_type" : "simple",
-                            "quantity"  : "1",
-                            "unit_name" : "ounces cheese curds or 6 ounces fresh mozzarella cheese, diced",
-
-                            "category"  : {
-                                "id" : "4",
-                                "name"   : "Drinks",
-                            }
-
-
-                        }
-                    ],
-                    "steps": [
-                        "",
-                        "",
-                        "",
-                        ""
-                    ],
-                    "meta": [{
-                        "prepTime"    :"PT30M",
-                        "totalTime"   :"PT3H",
-                        "recipeYield" :"8",
-                        "description" : "",
-                        "notes"       : ""
-                    }]
-                }
-
-                {
-
-  "name": "Perfect Apple Pie",
-  "author": "Gin Blanco",
-  "image": "http://images.edge-generalmills.com/56459281-6fe6-4d9d-984f-385c9488d824.jpg",
-  "description": "A classic apple pie takes a shortcut with easy Pillsbury® unroll-fill refrigerated pie crust.",
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": "4.5",
-    "reviewCount": "276",
-    "bestRating": "5",
-    "worstRating": "1"
-  },
-  "prepTime": "PT30M",
-  "totalTime": "PT3H",
-  "recipeYield": "8",
-  "nutrition": {
-    "@type": "NutritionInformation",
-    "servingSize": "1 medium slice",
-    "calories": "230 calories",
-    "fatContent": "1 g",
-    "carbohydrateContent": "43 g",
-    "cholesterolContent": "0 mg",
-    "fiberContent": "1 g",
-    "proteinContent": "1 g",
-    "saturatedFatContent": "2 ½ g",
-    "servingSize": "1 Serving",
-    "sodiumContent": "200 mg",
-    "sugarContent": "27 g",
-    "transFatContent": "0 g"
-  },
-  "recipeIngredient": [
-    "1 box Pillsbury™ refrigerated pie crusts, softened as directed on box",
-    "6 cups thinly sliced, peeled apples (6 medium)",
-    "3/4 cup sugar",
-    "2 tablespoons all-purpose flour",
-    "3/4 teaspoon ground cinnamon",
-    "1/4 teaspoon salt",
-    "1/8 teaspoon ground nutmeg",
-    "1 tablespoon lemon juice"
-  ],
-  "recipeInstructions": [
-    "1 Heat oven to 425°F. Place 1 pie crust in ungreased 9-inch glass pie plate. Press firmly against side and bottom.",
-    "2 In large bowl, gently mix filling ingredients; spoon into crust-lined pie plate. Top with second crust. Wrap excess top crust under bottom crust edge, pressing edges together to seal; flute. Cut slits or shapes in several places in top crust.",
-    "3 Bake 40 to 45 minutes or until apples are tender and crust is golden brown. Cover edge of crust with 2- to 3-inch wide strips of foil after first 15 to 20 minutes of baking to prevent excessive browning. Cool on cooling rack at least 2 hours before serving."
-   ]
-}
-
-{
-    "title": "Basic Fruit Salad",
-    "yields" : {
-        "servings": "6"
-    },
-    "ingredients": [
-        {
-          "name" : "apple",
-          "amounts" : {
-              "amount" : "1",
-              "unit"   : "each",
-              "quantity": 0.333333333333333,
-              "displayQuantity": "1/3",
-              "metricQuantity": 59,
-              "metricDisplayQuantity": "59",
-              "metricUnit": "ml",
-          }
-        },
-        {
-          "name" : "banana",
-          "amounts" : {
-              "amount" : "1",
-              "unit"   : "each",
-              "quantity": 0.333333333333333,
-              "displayQuantity": "1/3",
-              "metricQuantity": 59,
-              "metricDisplayQuantity": "59",
-              "metricUnit": "ml",
-          }
-        },
-        {
-          "name" : "orange",
-          "amounts" : {
-              "amount" : "1",
-              "unit"   : "each",
-              "quantity": 0.333333333333333,
-              "displayQuantity": "1/3",
-              "metricQuantity": 59,
-              "metricDisplayQuantity": "59",
-              "metricUnit": "ml",
-          }
-        },
-        {
-          "name" : "grapes",
-          "amounts" : {
-              "amount" : "10",
-              "unit"   : "cup",
-              "quantity": 0.333333333333333,
-              "displayQuantity": "1/3",
-              "metricQuantity": 59,
-              "metricDisplayQuantity": "59",
-              "metricUnit": "ml",
-          }
-        },
-    ],
-    "steps": [
-
-        {
-          "step"  : "Cut the apple into cubes",
-          "notes" : [
-              "Use whole apples",
-              "Pears may be substituted, but produce a different flavor and mouthfeel"
-          ]
-        },
-        {
-          "step"  : "Cut the banana into slices",
-          "notes" : [
-              "",
-              ""
-          ]
-        },
-        {
-          "step"  : "Peel the orange, and divide into segments",
-          "notes" : [
-              "",
-              ""
-          ]
-        },
-        {
-          "step"  : "Combine all ingredients in a bowl",
-          "notes" : [
-              "",
-              ""
-          ]
-        },
-        {
-          "step"  : "Mix to combine",
-          "notes" : [
-              "",
-              ""
-          ]
-        },
-
-    ],
-    "notes" : [
-        "This is a friendly recipe; giving, rather than throwing, is recommended.1",
-        "This is a friendly recipe; giving, rather than throwing, is recommended.2",
-        "This is a friendly recipe; giving, rather than throwing, is recommended.3"
-    ]
-
-}
-
-
-
-'use strict';
-
-module.exports = {
-
-    "id"   : "1989",
-    "name" : "Crock Pot Roast",
-    "ingredients": [
       {
-        "quantity": "1",
-        "name": " beef roast",
-        "department": "Meat"
+          title   : "Baked Chicken Chimichangas, Borracho Beans",
+          directions: [
+              "1",
+              "2",
+              "3",
+              "4"
+          ],
+          prep_time    :"PT30M",
+          total_time   :"PT3H",
+          recipe_yield :"8",
+          img: "http://img.sndimg.com/food/image/upload/h_479,w_639,c_fit/v1/img/recipes/51/78/46/BKN9rVETnqVjW3zddIJ1_0S9A4664.jpg",
+          url: "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208",
       },
       {
-        "quantity": "1 package",
-        "name": "brown gravy mix",
-        "department": "Baking"
-      },
-      {
-        "quantity": "1 package",
-        "name": "dried Italian salad dressing mix",
-        "department": "Condiments"
-      },
-      {
-        "quantity": "1 package",
-        "name": "dry ranch dressing mix",
-        "department": "Condiments"
-      },
-      {
-        "quantity": "1/2 cup",
-        "name": "water",
-        "department": "Drinks"
-      }
-    ],
-    "directions": [
-      "Place beef roast in crock pot.",
-      "Mix the dried mixes together in a bowl and sprinkle over the roast.",
-      "Pour the water around the roast.",
-      "Cook on low for 7-9 hours."
-    ],
-
-    "prepTime"    :"30min",
-    "totalTime"   :"3h",
-    "recipeYield" :"8",
-
-    "imageURL": "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",
-    "originalURL": "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208"
-};
-
-
-
-
-
-      {
-
-        title: "Crock Pot Roast12",
+        title: "Perfect Apple Pie",
         directions: [
-         "Place beef roast in crock pot.",
-          "Mix the dried mixes together in a bowl and sprinkle over the roast.",
-          "Pour the water around the roast.",
-          "Cook on low for 7-9 hours."
+            "1 Heat oven to 425°F. Place 1 pie crust in ungreased 9-inch glass pie plate. Press firmly against side and bottom.",
+            "2 In large bowl, gently mix filling ingredients; spoon into crust-lined pie plate. Top with second crust. Wrap excess top crust under bottom crust edge, pressing edges together to seal; flute. Cut slits or shapes in several places in top crust.",
+            "3 Bake 40 to 45 minutes or until apples are tender and crust is golden brown. Cover edge of crust with 2- to 3-inch wide strips of foil after first 15 to 20 minutes of baking to prevent excessive browning. Cool on cooling rack at least 2 hours before serving."
         ],
-        prep_time    :"30min",
-        total_time   :"3h",
+        prep_time    :"PT30M",
+        total_time   :"PT3H",
         recipe_yield :"8",
-        img: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",
-        "url": "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208",
-
+        img: "http://images.edge-generalmills.com/56459281-6fe6-4d9d-984f-385c9488d824.jpg",
+        url: "http://google.com",
       },
       {
-
-        title: "Crock Pot Roast1",
-        img: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",
-        url: "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208",
+        title: "Basic Fruit Salad",
+        directions: [
+            "Cut the apple into cubes",
+            "Cut the banana into slices",
+            "Peel the orange, and divide into segments",
+            "Combine all ingredients in a bowl",
+            "Mix to combine",
+        ],
+        prep_time    :"PT30M",
+        total_time   :"PT3H",
+        recipe_yield :"8",
+        img: "http://images.edge-generalmills.com/56459281-6fe6-4d9d-984f-385c9488d824.jpg",
+        url: "http://google.com",
+      },
+      {
+        title : "Crock Pot Roast",
         directions: [
           "Place beef roast in crock pot.",
           "Mix the dried mixes together in a bowl and sprinkle over the roast.",
           "Pour the water around the roast.",
           "Cook on low for 7-9 hours."
         ],
-        prep_time    :"PT30M",
-        total_time   :"PT3H",
-        recipe_yield :"8",
-
+        prep_time    :"30min",
+        total_time   :"3h",
+        recipe_yield :"4",
+        img: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",
+        url: "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208"
       }
+
      ];
 
   	return data;
