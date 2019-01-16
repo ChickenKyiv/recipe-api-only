3212787b6eebf15b132cf8d56561525f9ae69787
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-16 03:47:03 +0200

    keeping stuff in one place

--- bin/0002-automigrate.js
+++ bin/0002-automigrate.js
@@ -1,10 +1,10 @@
 // this will be our next version of automigrate script for loopback.
 
-var path     = require('path');
+const path     = require('path');
 
 let app      = require(path.resolve(__dirname, '../server/server'));
 
-const { db_name, lbTables } = require ('./0005-config.js');
+// const { db_name, lbTables } = require ('./0005-config.js');
 
 // console.log(server);
 // die();
@@ -14,6 +14,8 @@ const { db_name, lbTables } = require ('./0005-config.js');
 // var database = server.datasources[db_name];
 
 var database = app.datasources.recipeDS;
+
+console.log(database);
 
 // var database = app.datasources.searchDS;
 // var lbTables = [
@@ -25,15 +27,15 @@ var database = app.datasources.recipeDS;
 // ];
 
 //creating loopback necessary tables if no exists
-database.automigrate(lbTables, function(err) {
-  if (err) throw err;
-
-  console.log( 'Loopback tables [' + lbTables.toString() + '] created in ' + database.adapter.name );
-  database.disconnect();
-
-  // @TODO we can add something here - if we need this additional message...
-  // process.on('exit', function(code) {
-  // 	return console.log(`Automigrate is competed`);
-  // });
-  // process.exit(22);
-});
+// database.automigrate(lbTables, function(err) {
+//   if (err) throw err;
+//
+//   console.log( 'Loopback tables [' + lbTables.toString() + '] created in ' + database.adapter.name );
+//   database.disconnect();
+//
+//   // @TODO we can add something here - if we need this additional message...
+//   // process.on('exit', function(code) {
+//   // 	return console.log(`Automigrate is competed`);
+//   // });
+//   // process.exit(22);
+// });
